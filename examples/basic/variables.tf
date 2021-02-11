bf30e47b144340a842118498bce8e34dbb945212
Kleyton Henrique dos Santos <kleytonhsantos@gmail.com>
2021-02-11 15:01:59 -0300

    Include variable create_ecs

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -3,3 +3,9 @@ variable "aws_region" {}
 variable "ecs_cluster_name" {
   description = "The ECS Cluster Name"
 }
+
+variable "create_ecs" {
+  description = "Create a ECS Cluster?"
+  type        = bool
+  default     = false
+}
