f390d2e05af4b8f59abe512249bd6b05734e9ebf
Kleyton Henrique <kleytonhsantos@gmail.com>
2021-02-11 16:51:15 -0300

    [DSO-548] - Kubernetes in AWS (#5)
    * Include new variable create_ecs
    
    * Include count in module
    
    * Alter test module
    
    * Include variable create_ecs

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 resource "aws_ecs_cluster" "this" {
+  count              = var.create_ecs ? 1 : 0
   name               = var.ecs_cluster_name
   capacity_providers = var.ecs_capacity_providers
 
   dynamic "setting" {
+
     for_each = var.ecs_setting
     content {
       name  = setting.value.name
